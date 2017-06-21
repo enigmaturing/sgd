de0655d331732b5a954988f0e147b2a9c8dbdd8d
enigmaturing <javier.glez.martin@gmail.com>
2017-06-21 22:41:15 +0200

    Aufgabe 3.12 was added

---
+++ src/Aufgabe_3_12/KreisTest.java
@@ -0,0 +1,13 @@
+package Aufgabe_3_12;
+
+public class KreisTest {
+
+	public static void main(String[] args) {
+		Kreis kreis;
+		for (int i=0; i<5; i++){
+			kreis = new Kreis(100, 100, 50);
+		}
+		System.out.println(Kreis.getKreisZaehler() + " Kreis Objekte erzeugt.");
+	}
+	
+}
