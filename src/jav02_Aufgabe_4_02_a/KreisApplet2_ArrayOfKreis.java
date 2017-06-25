c094476f93b438554a80a71442f6bb9ab66003e6
enigmaturing <javier.glez.martin@gmail.com>
2017-06-25 13:28:45 +0200

    Adding some information

---
+++ src/jav02_Aufgabe_4_02_a/KreisApplet2_ArrayOfKreis.java
@@ -0,0 +1,16 @@
+package jav02_Aufgabe_4_02_a;
+
+public class KreisApplet2_ArrayOfKreis extends java.applet.Applet {
+	int anzahlAnKreise = 200;
+	Kreis[] kreis = new Kreis[anzahlAnKreise]; 				// declare an array of kreis-objects
+	public void init(){
+		for (int i=0; i<kreis.length; i++){
+			kreis[i] = new Kreis(130 + 2 * i, 130, 100);  	// initialize each kreis-object in the kreis-aray
+		}
+	}
+	public void paint(java.awt.Graphics g){
+		for (int i=0; i<kreis.length; i++){					//display each kreis-object present in the kreis-array
+			kreis[i].zeichne(g);
+		}
+	}
+}
