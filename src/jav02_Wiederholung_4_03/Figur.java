8cdb256afb0633429130eab8b6c57384cc0b476f
enigmaturing <javier.glez.martin@gmail.com>
2017-06-26 19:22:09 +0200

    Adding new exercises

---
+++ src/jav02_Wiederholung_4_03/Figur.java
@@ -0,0 +1,7 @@
+package jav02_Wiederholung_4_03;
+
+public class Figur {
+	protected int x, y;
+	public int getX() { return x; }
+	public int getY() { return y; }
+}
---
+++ src/jav02_Wiederholung_4_03/KreisTest.java
@@ -0,0 +1,10 @@
+package jav02_Wiederholung_4_03;
+
+public class KreisTest {
+
+	public static void main(String[] args) {
+		Kreis kreis = new Kreis(10, 10, 100);
+		System.out.println("X-Value der Objekt kreis: " + kreis.getX() + " Y-Value der Objekt kreis: " + kreis.getY());
+	}
+
+}
